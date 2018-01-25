06116c2b4074340f84f2ebfcd104bb50ff5ce363
lyokolux <29046067+Lyokolux@users.noreply.github.com>
2018-01-25 09:38:31 +0100

    Code improvments

--- smsMeteoMetz.py
+++ smsMeteoMetz.py
@@ -10,8 +10,8 @@ def parsing(pageHTML):
 			- temperature
 
 		data_* : extraction from the HTML page
-		message_* : list of the elements which are used in 
-			the final message
+		msg_* : list of the elements which are used in 
+			the final msg
 
 		return all data in a string
 	"""
@@ -19,52 +19,40 @@ def parsing(pageHTML):
 	content_page = content_page.find("ul", class_="prevision-horaire")
 
 	data_clock = content_page.find_all("time")
-	message_clock = []
-	for element in data_clock:
-		message_clock.append(element.get_text())
+	msg_clock = [element.get_text() for element in data_clock]
 
 	data_temperature = content_page.find_all("li", class_="day-summary-temperature")
-	message_temperature = []
-	count = 1
-	for element in data_temperature:
-		if count % 2 == 0:
-			message_temperature.append(element.get_text())
-		count += 1
+	msg_temperature = [temp.get_text() for (i, temp) in enumerate(data_temperature) if i % 2 == 1]
 
 	data_sky = content_page.find_all("li", class_="day-summary-image")
-	message_sky = []
-	count = 1
-	for element in data_sky:
-		if count % 2 == 0:
-			message_sky.append(element.find("span").get_text())
-		count += 1
-	message_sky.insert(0, content_page.find("li", class_="day-summary-label").get_text())
+	msg_sky = [content_page.find("li", class_="day-summary-label").get_text()]
+	for (i, element) in enumerate(data_sky):
+		if i % 2 == 1:
+			msg_sky.append(element.find("span").get_text())
 
-	message = '# Meteo Metz\n'
-	for i in range(len(message_temperature)):
-		if i <= len(message_sky) :
-			message += ("{} - {} - {} \n".format(message_clock[i], message_sky[i], message_temperature[i]))
 
-	return message
+	msg = '# Meteo Metz\n'
+	for i in range(len(msg_temperature)):
+		msg += (f"{msg_clock[i]} - {msg_sky[i]} - {msg_temperature[i]} \n")
+
+	return msg
 
 #---------------------#
 #  Reading 		      #
 #---------------------#
 
-pageFile = urlopen('http://www.meteofrance.com/'\
-	'previsions-meteo-france/metz/57000')
+pageFile = urlopen('http://www.meteofrance.com/previsions-meteo-france/metz/57000')
 pageHTML = pageFile.read()
-message = parsing(pageHTML)
+msg = parsing(pageHTML)
 pageFile.close()
 
 #---------------------#
 # Envoi du sms        #
 #---------------------#
-
 user='YOUR_USER'
 pas='YOUR_PASSWORD'
 
 #quote convert from utf-8 to ASCII
-texte = str(message)
+texte = str(msg)
 url = 'https://smsapi.free-mobile.fr/sendmsg?&user='+user+'&pass='+pas+'&msg='+quote(texte)
 reponse = urlopen(url)
